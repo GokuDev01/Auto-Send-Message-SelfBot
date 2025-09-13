841e0a01b1647a5930c01adbaddf40128a2d9850
Goku <rajdeepdutta090@gmail.com>
2025-09-13 17:59:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-DEFAULT_MIN_INTERVAL = float(os.getenv("DEFAULT_MIN_INTERVAL", "2.0"))
-
-if not BOT_TOKEN or OWNER_ID == 0:
-    raise SystemExit(
-        "Please set DISCORD_BOT_TOKEN and OWNER_ID correctly in .env"
-    )
-  
+{"userdata": {"token": "null", "hours": "null", "message": "null", "channelids": "null"}}
